f8e5682503848c3aa709c904467d6681da970e5c
venkatesh babu <venkateshdiyyala@gmail.com>
2025-11-05 20:42:30 +0000

    my first commit

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bobby"
+    bucket         = "my-terraform-state-bobby-venkat"
     region         = "us-east-1"
     key            = "s3-github-actions/terraform.tfstate"
     encrypt = true
--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-2"
 }
 
 #tfsec:ignore:aws-s3-enable-bucket-logging
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-example-bucket-999"
+  bucket = "my-tf-example-bucket-999-venkat"
 }
 
 resource "aws_s3_bucket_ownership_controls" "example" {
